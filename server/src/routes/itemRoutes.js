9b7ae8ad895af3cbbfa837fd512dcdaaebc38761
PedroQGomes <pedromqgomes@gmail.com>
2021-03-20 02:43:56 +0000

    adicionado exemplos de pedidos

--- server/src/routes/itemRoutes.js
+++ server/src/routes/itemRoutes.js
@@ -2,18 +2,20 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemControllers');
 
-// exemplo = localhost:5000/item/?id=PV18SN91645
 
 
-
+// exemplo = localhost:5000/item/category/VESTIDOS - 1600  
 router.get('/category/:cat', itemController.getItemsOfcategory);
 
-
+// exemplo = localhost: 5000 / item / name / TUNICA NÉVOA
 router.get('/name/:name', itemController.getItemByName);
 
 
+// exemplo = localhost:5000/item/PV18SN91645
 router.get('/:id', itemController.getItemByRef);
 
+
+// para teste apenas
 router.get('/teste/:id', itemController.teste);
 
 
