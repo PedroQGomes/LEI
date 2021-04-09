e9b560ccc30ddc6ee19c568aa835be92e121a859
PedroQGomes <pedromqgomes@gmail.com>
2021-04-09 17:44:56 +0100

    some auth update

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,24 +1,27 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, {useState,useContext,useEffect} from 'react';
 import { Input,Button } from "@chakra-ui/react";
 import '../App.css';
+import { useAuth } from '../context/AuthContext';
+import { Link, useHistory } from "react-router-dom";
 
-const Login = (props) => {
+const Login = () => {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
-  const handleSubmit = () => {
-    console.log(username);
-    console.log(password);
-    axios.post('/user/login',{
-        username: username,
-	      password: password
-    }).then((res) => {
-        console.log(res);
-        props.history.push('/');
-    
-    });
+  const { login } = useAuth();
+
+
+  async function handleSubmit(e) {
+
+      try {
+      await login(username, password);
+      history.push("/");
+    } catch {
+      console.log("Failed to log in");
+    }
 
   };
 
@@ -33,6 +36,14 @@ const Login = (props) => {
 
   };
 
+
+  if(loading){
+    return (
+      <div > 
+            loading...
+      </div>
+    );
+  }else{
  
     return (
       <form onSubmit={handleSubmit}> 
@@ -50,6 +61,7 @@ const Login = (props) => {
       </form>
       
     );
+  }
   
 };
 
