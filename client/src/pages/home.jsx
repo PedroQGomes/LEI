7bc5c92a524a1aa94bfa1e108f9f6da510ab0baa
PedroQGomes <pedromqgomes@gmail.com>
2021-04-23 14:20:05 +0100

    home page 1st version

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect,useContext } from 'react';
 import axios from 'axios';
-import { Input,Button } from "@chakra-ui/react";
+import { Input,Button,Box } from "@chakra-ui/react";
 import '../App.css';
 import { Link, useHistory } from "react-router-dom";
 
@@ -16,7 +16,17 @@ const Home = () => {
     
     return (
     <body className = 'home'>
-        <Button colorScheme='white' color='black' onClick={closeMobileMenu}>LogIn</Button>
+        <Button pos="absolute" top="60%" left="50%" colorScheme='blue' color='white' onClick={closeMobileMenu}>Join now</Button>
+        <div className = 'ferrache'>
+            Ferrache
+        </div>
+        <div className = 'title'>
+            Managing made easy
+        </div>
+        <div className = 'subtitle'>
+            Dashboards, searches and more.
+        </div>
+        <img className='homeImg' src={process.env.PUBLIC_URL + '/homeImg.png'} /> 
     </body>
     );
 }
